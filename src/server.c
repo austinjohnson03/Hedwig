1d204787d56898b652dc76c134ebceed1297ea9c
austinjohnson03 <amj102998@gmail.com>
2025-12-04 02:18:44 -0500

    refactored server logic to start dev

--- src/server.c
+++ src/server.c
@@ -1,10 +1,7 @@
-// #include "server.h"
-// #include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -15,22 +12,19 @@
 #define MAX_BACKLOG 64       // affects is how many incoming connections can queue up 
                              // if your application isn't accept()ing connections as soon as they come in.
 
-int main(void) {
-    struct sockaddr_in my_addr; 
-    int sock_fd, rv;
-    char buffer[BUFFER_SIZE] = { 0 };
-    char *hello = "Hello from server\n";
+int handle_client(int client_fd, char buffer[]);  // placeholder to avoid compiler whining
 
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+int server(void) {
+    struct sockaddr_in my_addr; 
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    int const sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sock_fd == -1) {
         fprintf(stderr, "Error initializing server socket.\n");
         exit(1);
     }
     
-    // bzero depricated
-    // bzero(&my_addr, sizeof(my_addr));
-
     memset(&my_addr, 0, sizeof(my_addr));
 
     my_addr.sin_family = AF_INET;
@@ -44,28 +38,26 @@ int main(void) {
         exit(1);
     }
 
-    int lv = listen(sock_fd, MAX_BACKLOG);
-    if (lv == -1) {
+    int const lv = listen(sock_fd, MAX_BACKLOG);
+    if (lv < 0) {
         fprintf(stderr, "Error listening for connections on server.\n");
         exit(1);
     }
 
-    int new_sock = accept(sock_fd, (struct sockaddr *)&my_addr, &addrlen);
+    while (1) {
+        int const new_sock = accept(sock_fd, (struct sockaddr *)&my_addr, &addrlen);
+        if (new_sock < 0) {
+            fprintf(stderr, "Error accepting connection from client\n");
+            continue;
+        }
 
-    if (new_sock < 0) {
-        fprintf(stderr, "Error accepting connection from client\n");
-        exit(1);
+        int const cv = handle_client(new_sock, buffer);
+
+        if (cv < 0) {
+            break;
+        }
     }
 
-    fprintf(stderr, "Reading from buffer.\n");
-    rv = read(new_sock, buffer, BUFFER_SIZE - 1);
-    if (rv < 0 ) {
-        fprintf(stderr, "Error reading from client.\n");
-        exit(1);
-    }
-    printf("%s\n", buffer);
-
-    close(new_sock);
     close(sock_fd);
 
     return 0;
